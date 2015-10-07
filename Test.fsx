8c3770e4df2cbd5bce75637e729d6b725a7c74a2
Taylor Wood <taylorwood@me.com>
2015-10-07 07:12:20 -0500

    Remove header comment.

--- Test.fsx
+++ Test.fsx
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-#load "MarkovText.fs"
+﻿#load "MarkovText.fs"
 #load "MarkovTextBuilder.fs"
 
 open System.IO
