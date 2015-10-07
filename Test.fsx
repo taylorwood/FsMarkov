305db8cee68ee1973615520544bc001d3ebe58dd
Taylor Wood <taylorwood@me.com>
2015-10-07 07:12:44 -0500

    Update file path to match blog post example.

--- Test.fsx
+++ Test.fsx
@@ -8,7 +8,7 @@ open FsMarkov.MarkovText
 // try different values of n
 let nGramSize = 3
 // get a sequence of n-grams from a text file
-let corpus = File.ReadAllText "/Users/Taylor/Projects/Raw Text/Picture of Dorian Gray.txt"
+let corpus = File.ReadAllText "/Users/Taylor/Projects/Raw Text/Metamorphosis.txt"
 let nGrams = getWordPairs nGramSize corpus
 // build the Markov string transition map
 let map = buildMarkovMap nGrams
